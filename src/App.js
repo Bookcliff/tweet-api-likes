cc7434af3170d7d1eca1e6c759bcda3483b214df
Bookcliff <bookcliff76@gmail.com>
2022-09-27 14:43:35 -0500

    add antd styling and table

--- src/App.js
+++ src/App.js
@@ -1,60 +1,62 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState, useRef } from "react";
 
-import { useEffect, useState } from "react";
+import { Table, Input, Layout, Row, PageHeader, Col } from "antd";
 
 // const people = require("./data");
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   const [users, setUsers] = useState([]);
   const [pagedUsers, setPagedUsers] = useState([]);
   const [pageToken, setPageToken] = useState();
-  // const [id, setId] = useState();
+  const [id, setId] = useState();
+  const inputRef = useRef(null);
 
-  const id = "1564962773284446208";
+  // const id = "1564962773284446208";
 
   // console.log(users);
-  console.log(pageToken);
+  // console.log(pageToken);
 
   // const { usersNames } = useData(id);
 
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fetch(`api/getApi/?id=${id}`);
-      const dataList = await data.json();
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const data = await fetch(`api/getApi/?id=${id}`);
+  //     const dataList = await data.json();
 
-      // console.log(dataList?.data.meta.next_token);
+  //     // console.log(dataList?.data.meta.next_token);
 
-      const likeUsers = dataList.data.data;
-      const userArray = likeUsers?.map((user) => user.username);
-      setUsers(userArray);
-      setPageToken(dataList?.data.meta.next_token);
-    };
-    getData();
-  }, [id]);
+  //     const likeUsers = dataList.data.data;
+  //     const userArray = likeUsers?.map((user) => user.username);
+  //     setUsers(userArray);
+  //     setPageToken(dataList?.data.meta.next_token);
+  //   };
+  //   getData();
+  // }, [id]);
 
-  useEffect(() => {
-    const getPagedData = async () => {
-      const page = true;
-      const pagedDataArray = [];
+  // useEffect(() => {
+  //   const getPagedData = async () => {
+  //     const page = true;
+  //     const pagedDataArray = [];
 
-      while (page === true) {
-        const pagedData = await fetch(
-          `api/getPagesApi/?id=${id}&paginationToken=${pageToken}`
-        );
-        const pagedDataList = await pagedData.json();
-        console.log(pagedDataList);
+  //     while (page === true) {
+  //       const pagedData = await fetch(
+  //         `api/getPagesApi/?id=${id}&paginationToken=${pageToken}`
+  //       );
+  //       const pagedDataList = await pagedData.json();
+  //       console.log(pagedDataList);
 
-        // const updatedArray = pagedDataArray.concat(pagedDataList);
-        setPageToken(pagedDataList?.data.meta.next_token);
-      }
-      if (!pageToken) {
-        const page = false;
-        return page;
-      }
-    };
-    getPagedData();
-  }, [id, pageToken]);
+  //       // const updatedArray = pagedDataArray.concat(pagedDataList);
+  //       setPageToken(pagedDataList?.data.meta.next_token);
+  //     }
+  //     if (!pageToken) {
+  //       const page = false;
+  //       return page;
+  //     }
+  //   };
+  //   getPagedData();
+  // }, [id, pageToken]);
 
   // const importantPeople = people.map((person) => person.username);
 
@@ -63,22 +65,52 @@ function App() {
   // );
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header>
+        <Row justify="space-between">
+          <Col></Col>
+          <Col> </Col>
+        </Row>
+      </Header>
+      <Content style={{ padding: "0 24px", marginTop: 16 }}>
+        <PageHeader
+          style={{ backgroundColor: "#fff" }}
+          title="Pre-Identified Users Tweet-Like List"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Enter a tweet id below to see the list of pre-identified users that
+          liked it.{" "}
+        </PageHeader>
+
+        <div style={{ background: "#fff", padding: 24 }}>
+          <div>
+            <Input
+              placeholder="Tweet ID"
+              value={""}
+              onChange={(event) => {
+                setId(event.target.value);
+              }}
+              ref={inputRef}
+            />
+          </div>
+
+          {/* Added math.random() because the keys were not unique which was messing with the blockNumber sorting */}
+          {id && (
+            <Table
+            // style={{ marginTop: 24 }}
+            // pagination={false}
+            // rowKey={(record, index) =>
+            //   record.logIndex + Math.random() * index
+            // }
+            // columns={createColumns(filter)}
+            // dataSource={updatedEventsTime}
+            // loading={isLoading}
+            // scroll={{ x: 400 }}
+            />
+          )}
+        </div>
+      </Content>
+      <Footer></Footer>
+    </Layout>
   );
 }
 

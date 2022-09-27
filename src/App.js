bd1123677b9d23bdc33afa5fdad638d408a57ea9
Bookcliff <bookcliff76@gmail.com>
2022-09-27 14:24:46 -0500

    change api calls to use fetch

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import useSWR from "swr";
 import { useEffect, useState } from "react";
 
-const people = require("./data");
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-function useData(id) {
-  const { data, error } = useSWR(`api/getApi/?id=${id}`, fetcher);
-
-  return {
-    usersNames: data,
-    isLoading: !error & !data,
-    isError: error,
-  };
-}
+// const people = require("./data");
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [pagedUsers, setPagedUsers] = useState([]);
+  const [pageToken, setPageToken] = useState();
   // const [id, setId] = useState();
 
-  const id = "1354143047324299264";
+  const id = "1564962773284446208";
 
-  const { usersNames } = useData(id);
+  // console.log(users);
+  console.log(pageToken);
 
-  const importantPeople = people.map((person) => person.username);
-
-  const intersectionUsername = users.filter((element) =>
-    importantPeople.includes(element)
-  );
-
-  console.log(intersectionUsername);
+  // const { usersNames } = useData(id);
 
   useEffect(() => {
-    const getLikes = async () => {
-      if (!id || !usersNames) {
-        setUsers([]);
-        return;
-      }
-      const likeUsers = usersNames.data.data;
-      const userArray = likeUsers.map((user) => user.username);
+    const getData = async () => {
+      const data = await fetch(`api/getApi/?id=${id}`);
+      const dataList = await data.json();
+
+      // console.log(dataList?.data.meta.next_token);
+
+      const likeUsers = dataList.data.data;
+      const userArray = likeUsers?.map((user) => user.username);
       setUsers(userArray);
+      setPageToken(dataList?.data.meta.next_token);
     };
-    getLikes();
-  }, [id, usersNames]);
+    getData();
+  }, [id]);
+
+  useEffect(() => {
+    const getPagedData = async () => {
+      const page = true;
+      const pagedDataArray = [];
+
+      while (page === true) {
+        const pagedData = await fetch(
+          `api/getPagesApi/?id=${id}&paginationToken=${pageToken}`
+        );
+        const pagedDataList = await pagedData.json();
+        console.log(pagedDataList);
+
+        // const updatedArray = pagedDataArray.concat(pagedDataList);
+        setPageToken(pagedDataList?.data.meta.next_token);
+      }
+      if (!pageToken) {
+        const page = false;
+        return page;
+      }
+    };
+    getPagedData();
+  }, [id, pageToken]);
+
+  // const importantPeople = people.map((person) => person.username);
+
+  // const intersectionUsername = users.filter((element) =>
+  //   importantPeople.includes(element)
+  // );
 
   return (
     <div className="App">

d6e5e6ae4c8c55faca1c07f0234c44124302dcf9
Bookcliff <bookcliff76@gmail.com>
2022-09-27 18:42:00 -0500

    Add data to table

--- src/App.js
+++ src/App.js
@@ -2,67 +2,98 @@ import { useEffect, useState, useRef } from "react";
 
 import { Table, Input, Layout, Row, PageHeader, Col } from "antd";
 
-// const people = require("./data");
+const people = require("./data");
 
 const { Header, Footer, Content } = Layout;
 
 function App() {
   const [users, setUsers] = useState([]);
   const [pagedUsers, setPagedUsers] = useState([]);
-  const [pageToken, setPageToken] = useState();
-  const [id, setId] = useState();
+  // const [id, setId] = useState();
   const inputRef = useRef(null);
 
-  // const id = "1564962773284446208";
+  const id = "1564962773284446208";
 
-  // console.log(users);
-  // console.log(pageToken);
+  const createColumns = () => [
+    {
+      title: "Username",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Position",
+      dataIndex: "position",
+      key: "position",
+    },
+    {
+      title: "Twitter Address",
+      key: "address",
+      render: (intersectionUsername) => {
+        return (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={intersectionUsername.address}
+          >
+            {intersectionUsername.address}
+          </a>
+        );
+      },
+    },
+  ];
 
-  // const { usersNames } = useData(id);
+  useEffect(() => {
+    const getPagedData = async () => {
+      let pagedDataArray = [];
+      let morePagesAvailable = true;
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const data = await fetch(`api/getApi/?id=${id}`);
-  //     const dataList = await data.json();
+      const originalData = await fetch(`api/getApi/?id=${id}`);
+      const dataList = await originalData.json();
+      let pageToken = dataList?.data?.meta.next_token;
 
-  //     // console.log(dataList?.data.meta.next_token);
+      const likeUsers = dataList.data.data;
+      const userArray = likeUsers?.map((user) => user.username);
+      setUsers(userArray);
 
-  //     const likeUsers = dataList.data.data;
-  //     const userArray = likeUsers?.map((user) => user.username);
-  //     setUsers(userArray);
-  //     setPageToken(dataList?.data.meta.next_token);
-  //   };
-  //   getData();
-  // }, [id]);
+      while (morePagesAvailable) {
+        const response = await fetch(
+          `api/getPagesApi/?id=${id}&paginationToken=${pageToken}`
+        );
+        const { data } = await response.json();
+        const fullData = data.data;
+        if (!fullData) {
+          setPagedUsers(pagedDataArray);
+        } else {
+          fullData.forEach((e) => pagedDataArray.push(e));
+        }
+        pageToken = data?.meta.next_token;
+        console.log(pageToken);
+        if (!pageToken) {
+          morePagesAvailable = false;
+        }
+      }
+    };
+    getPagedData();
+  }, [id]);
 
-  // useEffect(() => {
-  //   const getPagedData = async () => {
-  //     const page = true;
-  //     const pagedDataArray = [];
+  //Get usernames from twitter data
 
-  //     while (page === true) {
-  //       const pagedData = await fetch(
-  //         `api/getPagesApi/?id=${id}&paginationToken=${pageToken}`
-  //       );
-  //       const pagedDataList = await pagedData.json();
-  //       console.log(pagedDataList);
+  const pagesTwitterUsers = pagedUsers.map((user) => user.username);
 
-  //       // const updatedArray = pagedDataArray.concat(pagedDataList);
-  //       setPageToken(pagedDataList?.data.meta.next_token);
-  //     }
-  //     if (!pageToken) {
-  //       const page = false;
-  //       return page;
-  //     }
-  //   };
-  //   getPagedData();
-  // }, [id, pageToken]);
+  //Combine both twitter datasets
+
+  const fullLikesArray = users.concat(pagesTwitterUsers);
+  console.log({ pagedUsers, fullLikesArray });
+
+  //Pull out the usernames of the pre-defined people list
 
   // const importantPeople = people.map((person) => person.username);
 
-  // const intersectionUsername = users.filter((element) =>
-  //   importantPeople.includes(element)
-  // );
+  //Create array of pre-defined people who liked the tweet
+
+  const intersectionUsername = people.filter((element) =>
+    fullLikesArray.includes(element.username)
+  );
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
@@ -75,10 +106,11 @@ function App() {
       <Content style={{ padding: "0 24px", marginTop: 16 }}>
         <PageHeader
           style={{ backgroundColor: "#fff" }}
-          title="Pre-Identified Users Tweet-Like List"
+          title="Pre-Identified Users who Liked a Tweet"
         >
           Enter a tweet id below to see the list of pre-identified users that
-          liked it.{" "}
+          liked it. The tweet id is the string of numbers within the tweet's URL
+          on twitter (e.g., https://twitter.com/GoGoPool_/status/TWEET_ID)
         </PageHeader>
 
         <div style={{ background: "#fff", padding: 24 }}>
@@ -86,9 +118,9 @@ function App() {
             <Input
               placeholder="Tweet ID"
               value={""}
-              onChange={(event) => {
-                setId(event.target.value);
-              }}
+              // onChange={(event) => {
+              //   setId(event.target.value);
+              // }}
               ref={inputRef}
             />
           </div>
@@ -96,15 +128,12 @@ function App() {
           {/* Added math.random() because the keys were not unique which was messing with the blockNumber sorting */}
           {id && (
             <Table
-            // style={{ marginTop: 24 }}
-            // pagination={false}
-            // rowKey={(record, index) =>
-            //   record.logIndex + Math.random() * index
-            // }
-            // columns={createColumns(filter)}
-            // dataSource={updatedEventsTime}
-            // loading={isLoading}
-            // scroll={{ x: 400 }}
+              style={{ marginTop: 24 }}
+              pagination={false}
+              rowKey={(record) => record.logIndex}
+              columns={createColumns()}
+              dataSource={intersectionUsername}
+              scroll={{ x: 400 }}
             />
           )}
         </div>
